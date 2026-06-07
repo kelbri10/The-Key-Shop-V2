ece782f1a5e6e17dede35cefc26581562d8c93b7
Max Turkin <turkins@Maxs-MacBook-Air.local>
2026-06-06 21:44:03 -0400

    [refactor] cleans up unused components, adds landing page + nav

---
+++ the-key-shop-v2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './Components/Nav';
+import Landing from "./Pages/Landing"; 
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Landing /> 
+    </>
+  )
+}
+
+export default App
