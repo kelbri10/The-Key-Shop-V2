5ccc3debaba701f1cb768d980dc36d023ae3e434
Max Turkin <turkins@Maxs-MacBook-Air.local>
2026-06-06 21:33:30 -0400

    [feat]conditional rendering based on user sign up

--- the-key-shop-v2/src/Components/DiscountModal.jsx
+++ the-key-shop-v2/src/Components/DiscountModal.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react"; 
 
+
 const DiscountModal  = () => { 
-    const[email, setEmail] = useState(); 
+    const[email, setEmail] = useState(''); 
+    const[isSignedUp, setIsSignedUp] = useState(false); 
     const dialogRef = useRef(null) //where dialog reference refers to the dialog html element node
 
     const handleSubmit = (e) => {
-        e.preventDefault(); 
-        if(email === ""){ 
-            console.log('Please enter an email');   
-        }
-        console.log(`Thank you for signing up. Check your email ${email} to get a thank you discount on us!`); 
+        console.log(email);
+        setIsSignedUp(true); 
     }
 
     return (
@@ -18,25 +17,32 @@ const DiscountModal  = () => {
             <button id="dialog-toggle" onClick={()=> dialogRef.current.showModal()}>Show Discount Offer</button>
 
             <dialog ref={dialogRef}>
-                <h1> Become a member of the Insider Club! </h1>
-                <p> Sign up for free to our Inside Club and get access to special rewards! 20% off next purchase with special code to your inbox </p>
+                {
+                    isSignedUp ? (
+                    <>
+                        <h1>Thank you for signing up, {email}</h1>
+                        <p>Check your email for a special discount code!</p>
+                        <button id="dialog-toggle" onClick={()=> dialogRef.current.close()}>Close</button>
+                    </> 
+                    ) : (
+                    <>
+                        <h1> Become a member of the Insider Club! </h1>
+                        <p> Sign up for free to our Inside Club and get access to special rewards! 20% off next purchase with special code to your inbox </p>
 
-                <label htmlFor="email">Email:</label>
-                <input 
-                    type="email" 
-                    name="email" 
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} />
-                <input 
-                    type="submit" 
-                    name="submit-email" 
-                    id="submit-email" 
-                    value="Sign me up!" 
-                    onSubmit={(e)=> handleSubmit(e)} />
+                        <label htmlFor="email">Email:</label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)} />
+                        <button onClick={() => handleSubmit()}>Submit</button>
 
 
-                <button id="dialog-toggle" onClick={()=> dialogRef.current.close()}>No Thanks!</button>
+                        <button id="dialog-toggle" onClick={()=> dialogRef.current.close()}>No Thanks!</button>
+                    </>
+                    )
+                }
             </dialog>
         </> 
     )
