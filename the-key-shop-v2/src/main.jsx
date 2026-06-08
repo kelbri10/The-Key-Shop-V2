c601470ad685240226168df6da1fe5ec0944d886
Max Turkin <turkins@Maxs-MacBook-Air.local>
2026-06-07 23:38:54 -0400

    add store wrapper and browser router wrapper for navigation and state management

--- the-key-shop-v2/src/main.jsx
+++ the-key-shop-v2/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route} from "react-router"; 
+import store from "./redux/store.js"; 
+import Products from './Pages/Products.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
