335c4d8fc3c18c46f7a563965049396aa1acd84e
Max Turkin <turkins@Maxs-MacBook-Air.local>
2026-06-07 23:43:10 -0400

    [feat] add imports, console log products to ensure props passed

---
+++ the-key-shop-v2/src/Components/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {useDispatch } from "react-redux"; 
+
+const ProductList = ({products, filter = []}) => { 
+
+    console.log(products)
+    
+    return( 
+        <>
+            <h1> Products </h1>
+        </>
+    )
+}
+
+export default ProductList; 
