32de39b1713ca4b9f35440da40b9e8b74cf0c525
Kelsey Hughes <kelbri10@gmail.com>
2026-06-09 07:47:49 -0400

    [feat] adds state to track filter
    [feat] iterates through products to generate checkboxes to track filtered products

--- the-key-shop-v2/src/Pages/Products.jsx
+++ the-key-shop-v2/src/Pages/Products.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
 import ProductList from "../Components/ProductList";
+import { filterDisplay } from "../redux/ProductSlice";
 
 const Products = () => { 
+    const [filters, setFilters] = useState([]); 
+
     const products = useSelector(state => state.products)
+    const dispatch = useDispatch(); 
+
+    const handleFilter = () => { 
+        useDispatch(filterDisplay(filters)); 
+    }
     return (
-        <ProductList products={products} filter={[]} />
+        // there needs to be a component here that handles the checkbox and the filter to affect products and pass the filtered/unflitered products to product list
+        <section>
+            <h1>Products</h1>
+            <fieldset> 
+                <legend>Filter products: </legend>
+                    <div>
+                        <input type="checkbox" id="all" name="all" />
+                        <label htmlFor="all">All</label>
+                    </div>
+                    {products.map((cluster, index) => 
+                        (
+                            <div key={index}>
+                                <input type="checkbox" id={cluster.category} name={cluster.category}/>
+                                <label htmlFor={cluster.category}>{cluster.category}</label>
+                            </div>
+                        )
+                    )}
+            </fieldset>
+            <ProductList products={products} />
+        </section>
     )
 }
 
