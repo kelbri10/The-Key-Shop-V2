dbcefd7b6109c951885165c57a371727c42cf417
Kelsey Hughes <kelbri10@gmail.com>
2026-06-14 15:11:24 -0400

    updates imports

--- the-key-shop-v2/src/App.jsx
+++ the-key-shop-v2/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Nav from './Components/Nav';
+import Nav from './features/Common/Nav';
 import Landing from "./Pages/Landing"; 
 
 function App() {
