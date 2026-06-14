a5bf6df9c4e57632454cf36153bbe039cb2b4928
Kelsey Hughes <kelbri10@gmail.com>
2026-06-14 15:13:19 -0400

    [feat] adds handle change, displayProduct state, passes displayProduct state to ProductList component

--- the-key-shop-v2/src/Pages/Products.jsx
+++ the-key-shop-v2/src/Pages/Products.jsx
@@ -1,39 +1,57 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import ProductList from "../Components/ProductList";
-import { filterDisplay } from "../redux/ProductSlice";
+import ProductList from "../features/Products/ProductList";
+import { filterDisplay, selectProducts} from "../redux/ProductSlice";
 
 const Products = () => { 
+    const allProducts = useSelector(selectProducts)
+    
     const [filters, setFilters] = useState([]); 
-
-    const products = useSelector(state => state.products)
+    const [displayProducts, setDisplayProducts] = useState(allProducts); 
+    
     const dispatch = useDispatch(); 
 
-    const handleFilter = () => { 
-        useDispatch(filterDisplay(filters)); 
+
+    const handleChange = (e) => { 
+        let updatedFilters = []; 
+
+        if (e.target.checked){ 
+            updatedFilters = [...filters, e.target.value]; 
+        } else {
+            updatedFilters = filters.filter(cat => cat !== e.target.value); 
+        }
+        setFilters(updatedFilters);  
+        
     }
+
+    useEffect(() => { 
+        let filtered = []; 
+        if(filters.length > 0){
+            filtered = allProducts.filter(product => filters.includes(product.category)); 
+            setDisplayProducts(filtered); 
+        } else { 
+            setDisplayProducts(allProducts); 
+        }
+        
+    }, [filters])
     return (
         // there needs to be a component here that handles the checkbox and the filter to affect products and pass the filtered/unflitered products to product list
         <section>
             <h1>Products</h1>
             <fieldset> 
                 <legend>Filter products: </legend>
-                    <div>
-                        <input type="checkbox" id="all" name="all" />
-                        <label htmlFor="all">All</label>
-                    </div>
-                    {products.map((cluster, index) => 
+                    {allProducts.map((cluster, index) => 
                         (
                             <div key={index}>
-                                <input type="checkbox" id={cluster.category} name={cluster.category}/>
+                                <input type="checkbox" id={cluster.category} name={cluster.category} value={cluster.category}
+                                onChange={(e) => handleChange(e)}/>
                                 <label htmlFor={cluster.category}>{cluster.category}</label>
                             </div>
                         )
                     )}
             </fieldset>
-            <ProductList products={products} />
+            <ProductList products={displayProducts} />
         </section>
     )
 }
