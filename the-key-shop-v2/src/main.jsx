6957768f8686b7a4ca4da9b1aaf325f27e3017cb
Kelsey Hughes <kelbri10@gmail.com>
2026-06-17 22:33:25 -0400

    [chore] adds products and checkout to routes

--- the-key-shop-v2/src/main.jsx
+++ the-key-shop-v2/src/main.jsx
@@ -6,14 +6,16 @@ import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route} from "react-router"; 
 import store from "./redux/store.js"; 
 import Products from './Pages/Products.jsx'
+import Checkout from './Pages/Checkout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/products' element={<Products />} />
+          <Route path="/" element={<App />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </BrowserRouter>
     </Provider>
