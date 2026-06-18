cafdd56855f29883f2409db57f7d1fca83db860e
Kelsey Hughes <kelbri10@gmail.com>
2026-06-17 22:35:31 -0400

    [feat] adds low fidelity product card with placeholder img

--- the-key-shop-v2/src/features/Products/ProductCard.jsx
+++ the-key-shop-v2/src/features/Products/ProductCard.jsx
@@ -1,1 +1,23 @@
 import React from "react";
+import {useDispatch, useSelector } from "react-redux"; 
+import { addToCart, selectCart } from "../../redux/CartSlice";
+
+const ProductCard = ({product}) => { 
+    const dispatch = useDispatch(); 
+    
+    const {name, img, price} = product; 
+    const handleClick = ({name, img, price}) => { 
+        dispatch(addToCart({name, img, price})); 
+    }
+
+    return(
+        <div>
+            <img src="https://placehold.co/600x400" alt="placeholder image with 600 x 400 px display" />
+            <h3>{name}</h3>
+            <p>{price === "" ? "23.99" : price}</p>
+            <button onClick={() => handleClick({name, img, price})}>Add to Cart</button>
+        </div>
+    )
+}
+
+export default ProductCard; 
